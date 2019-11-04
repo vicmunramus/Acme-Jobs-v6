6b24466ba4ed2e2d872395016b4de39c62b99153
alval <alval@LAPTOP-GKUN0RT8>
2019-11-04 21:21:35 +0100

    Dashboard

---
+++ src/main/java/acme/forms/Dashboard.java
@@ -0,0 +1,20 @@
+
+package acme.forms;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Dashboard implements Serializable {
+
+	private static final long	serialVersionUID	= 1L;
+
+	//Attributes
+	Integer						numberInvestorRecordFirstSector;
+	Integer						numberInvestorRecordSecondSector;
+	Integer						numberInvestorRecordThirdSector;
+
+}
