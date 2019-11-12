979772dd644c5997f38053437ee5dcddb6ffe94d
xisco <xisco@DESKTOP-5O6B0KG.home>
2019-11-12 19:51:06 +0100

    Provider can create new requests.

---
+++ src/main/java/acme/features/provider/request/ProviderRequestRepository.java
@@ -0,0 +1,21 @@
+
+package acme.features.provider.request;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.requests.Request;
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface ProviderRequestRepository extends AbstractRepository {
+
+	@Query("select r from Request r where r.id = ?1")
+	Request findOneById(int id);
+
+	@Query("select r from Request r where r.deadline>utc_timestamp()")
+	Collection<Request> findManyAll();
+
+}
