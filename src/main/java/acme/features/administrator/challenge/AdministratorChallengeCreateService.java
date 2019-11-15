0bdb7a9335bdced8bcc5013a76919433d8a2a049
VICTOR MUÑOZ RAMÍREZ <VICTOR MUOZ RAMREZ@dircom.manten.us.es>
2019-11-15 15:01:37 +0100

    Task-06:
    Validate methods updated.

--- src/main/java/acme/features/administrator/challenge/AdministratorChallengeCreateService.java
+++ src/main/java/acme/features/administrator/challenge/AdministratorChallengeCreateService.java
@@ -2,7 +2,6 @@
 package acme.features.administrator.challenge;
 
 import java.util.Date;
-import java.util.stream.Collector;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +10,7 @@ import acme.entities.challenges.Challenge;
 import acme.framework.components.Errors;
 import acme.framework.components.Model;
 import acme.framework.components.Request;
+import acme.framework.datatypes.Money;
 import acme.framework.entities.Administrator;
 import acme.framework.services.AbstractCreateService;
 
@@ -64,36 +64,31 @@ public class AdministratorChallengeCreateService implements AbstractCreateServic
 		//DEADLINE
 		if (!errors.hasErrors("deadline")) {
 			boolean isAfter;
-			isAfter = request.getModel().getDate("deadline").after(new Date(System.currentTimeMillis()));
+			isAfter = entity.getDeadline().after(new Date(System.currentTimeMillis()));
 			errors.state(request, isAfter, "deadline", "administrator.challenge.error.deadline-must-be-in-the-future");
 		}
 
 		//REWARDS
 		if (!errors.hasErrors("bronzeReward")) {
-			String bronzeReward = request.getModel().getString("bronzeReward");
-			errors.state(request, this.inEuros(bronzeReward), "bronzeReward", "administrator.challenge.error.reward-must-be-in-euros");
+			boolean bronzeInEuros;
+			Money bronzeReward = entity.getBronzeReward();
+			bronzeInEuros = bronzeReward.getCurrency().equals("€") || bronzeReward.getCurrency().equals("EUR");
+			errors.state(request, bronzeInEuros, "bronzeReward", "administrator.challenge.error.reward-must-be-in-euros");
 		}
 
 		if (!errors.hasErrors("silverReward")) {
-			String silverReward = request.getModel().getString("silverReward");
-			errors.state(request, this.inEuros(silverReward), "silverReward", "administrator.challenge.error.reward-must-be-in-euros");
+			boolean silverInEuros;
+			Money silverReward = entity.getSilverReward();
+			silverInEuros = silverReward.getCurrency().equals("€") || silverReward.getCurrency().equals("EUR");
+			errors.state(request, silverInEuros, "silverReward", "administrator.challenge.error.reward-must-be-in-euros");
 		}
 
 		if (!errors.hasErrors("goldReward")) {
-			String goldReward = request.getModel().getString("goldReward");
-			errors.state(request, this.inEuros(goldReward), "goldReward", "administrator.challenge.error.reward-must-be-in-euros");
+			boolean goldInEuros;
+			Money goldReward = entity.getGoldReward();
+			goldInEuros = goldReward.getCurrency().equals("€") || goldReward.getCurrency().equals("EUR");
+			errors.state(request, goldInEuros, "goldReward", "administrator.challenge.error.reward-must-be-in-euros");
 		}
-	}
-
-	private boolean inEuros(final String reward) {
-		String comma = ",";
-		String point = ".";
-		String currency;
-
-		currency = reward.chars().mapToObj(c -> (char) c).filter(x -> !Character.isDigit(x) && !x.equals(comma.charAt(0)) && !x.equals(point.charAt(0)))
-			.collect(Collector.of(StringBuilder::new, StringBuilder::append, StringBuilder::append, StringBuilder::toString)).trim();
-
-		return currency.equals("€") || currency.equals("EUR");
 	}
 
 	@Override
