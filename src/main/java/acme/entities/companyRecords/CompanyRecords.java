8b44e8d36dd01b7fcf926da9a8ed837cb1e73386
enri8 <enri8@10.100.194.249>
2019-11-20 19:00:49 +0100

    Fixes

--- src/main/java/acme/entities/companyRecords/CompanyRecords.java
+++ src/main/java/acme/entities/companyRecords/CompanyRecords.java
@@ -4,6 +4,8 @@ package acme.entities.companyRecords;
 import java.beans.Transient;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -19,6 +21,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "rating")
+})
 public class CompanyRecords extends DomainEntity {
 
 	private static final long	serialVersionUID	= 1L;
