7e8e8456d52fa7570d8411c0615608bb4d243e11
xisco <xisco@DESKTOP-5O6B0KG.home>
2019-12-01 17:59:41 +0100

    authorise updated

--- src/main/java/acme/features/authenticated/messageThread/AuthenticatedMessageThreadRepository.java
+++ src/main/java/acme/features/authenticated/messageThread/AuthenticatedMessageThreadRepository.java
@@ -14,9 +14,16 @@ public interface AuthenticatedMessageThreadRepository extends AbstractRepository
 
 	@Query("select m from MessageThread m")
 	Collection<MessageThread> findManyMessageThreads();
+
 	@Query("select m from Authenticated a JOIN a.messageThread m WHERE a.id = ?1")
 	Collection<MessageThread> findManyMessageThreadByAuthenticatedId(int id);
+
 	@Query("select m from MessageThread m where m.id = ?1")
 	MessageThread findOneMessageThreadById(int id);
 
+	@Query("select a.id from Authenticated a where a.messageThread.id = ?1")
+	Collection<Integer> findManyAuthenticatedByMessageThreads(int id);
+
+	@Query("select COUNT(a) from Authenticated a where a.id = ?1 and a.messageThread.id = ?2")
+	Integer existsAuthenticatedIdByMessageThreads(int id, int messageThreadId);
 }
