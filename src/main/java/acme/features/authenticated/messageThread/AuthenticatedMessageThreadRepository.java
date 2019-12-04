2678bd91c69af9e25980551e5134d2e2424f8a2a
xisco <xisco@DESKTOP-5O6B0KG.home>
2019-12-04 19:10:40 +0100

    updated threads?

--- src/main/java/acme/features/authenticated/messageThread/AuthenticatedMessageThreadRepository.java
+++ src/main/java/acme/features/authenticated/messageThread/AuthenticatedMessageThreadRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.entities.messageThreads.MessageThread;
-import acme.framework.entities.Authenticated;
 import acme.framework.repositories.AbstractRepository;
 
 @Repository
@@ -18,11 +17,18 @@ public interface AuthenticatedMessageThreadRepository extends AbstractRepository
 
 	//	@Query("select m from MessageThread m WHERE m.creator.id = ?1")
 	//	Collection<MessageThread> findManyMessageThreadByAuthenticatedId(int id);
+	//select m.messageThread from Message m WHERE m.user.id = ?1
+	//select mt from MessageThread mt WHERE mt.creator.id = ?1
 	@Query("select m.messageThread from Message m WHERE m.user.id = ?1")
 	Collection<MessageThread> findManyMessageThreadByAuthenticatedId(int id);
 
-	@Query("select m.user from Message m WHERE m.messageThread.id = ?1")
-	Collection<Authenticated> findManyAuthenticatedByMessageThreadId(int id);
+	@Query("select m from MessageThread m WHERE m.creator.id = ?1")
+	MessageThread findOneMessageThreadByAuthenticatedId(int id);
+	//	@Query("select m.user from Message m WHERE m.messageThread.id = ?1")
+	//	Collection<Authenticated> findManyAuthenticatedByMessageThreadId(int id);
+
+	@Query("select COUNT(m) from Message m WHERE m.messageThread.id = ?1 AND m.user.id = ?2")
+	Integer existAuthenticatedByMessageThreadId(int messageThreadId, int userId);
 
 	@Query("select m from MessageThread m where m.id = ?1")
 	MessageThread findOneMessageThreadById(int id);
