28075f7a128c9562b4be3c31a67eb3d5392188bd
xisco <xisco@DESKTOP-5O6B0KG.home>
2019-12-04 12:34:29 +0100

    udpated threads

--- src/main/java/acme/features/authenticated/messageThread/AuthenticatedMessageThreadRepository.java
+++ src/main/java/acme/features/authenticated/messageThread/AuthenticatedMessageThreadRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.entities.messageThreads.MessageThread;
+import acme.framework.entities.Authenticated;
 import acme.framework.repositories.AbstractRepository;
 
 @Repository
@@ -15,8 +16,13 @@ public interface AuthenticatedMessageThreadRepository extends AbstractRepository
 	@Query("select m from MessageThread m WHERE m.id = ?1")
 	MessageThread findOneMessageThread(int msgThreadId);
 
-	@Query("select m from MessageThread m WHERE m.creator.id = ?1")
+	//	@Query("select m from MessageThread m WHERE m.creator.id = ?1")
+	//	Collection<MessageThread> findManyMessageThreadByAuthenticatedId(int id);
+	@Query("select m.messageThread from Message m WHERE m.user.id = ?1")
 	Collection<MessageThread> findManyMessageThreadByAuthenticatedId(int id);
+
+	@Query("select m.user from Message m WHERE m.messageThread.id = ?1")
+	Collection<Authenticated> findManyAuthenticatedByMessageThreadId(int id);
 
 	@Query("select m from MessageThread m where m.id = ?1")
 	MessageThread findOneMessageThreadById(int id);
