1ae4ce5cd251c149a1fef0e1edac6d4a75906fb7
xisco <xisco@DESKTOP-5O6B0KG.home>
2019-12-09 12:47:21 +0100

    Worker can create application but its still not verified

---
+++ src/main/java/acme/features/worker/application/WorkerApplicationCreateService.java
@@ -0,0 +1,85 @@
+
+package acme.features.worker.application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.jobs.Application;
+import acme.entities.jobs.Job;
+import acme.entities.roles.Worker;
+import acme.framework.components.Errors;
+import acme.framework.components.HttpMethod;
+import acme.framework.components.Model;
+import acme.framework.components.Request;
+import acme.framework.services.AbstractCreateService;
+
+@Service
+public class WorkerApplicationCreateService implements AbstractCreateService<Worker, Application> {
+
+	@Autowired
+	WorkerApplicationRepository repository;
+
+
+	@Override
+	public boolean authorise(final Request<Application> request) {
+		assert request != null;
+		boolean result = true;
+
+		return result;
+	}
+
+	@Override
+	public void bind(final Request<Application> request, final Application entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+		request.bind(entity, errors);
+
+	}
+
+	@Override
+	public void unbind(final Request<Application> request, final Application entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "reference", "moment", "status", "statement", "skills", "qualifications");
+
+		if (request.isMethod(HttpMethod.GET)) {
+			model.setAttribute("jobId", request.getModel().getInteger("jobId"));
+		}
+	}
+
+	@Override
+	public Application instantiate(final Request<Application> request) {
+		Application result = new Application();
+
+		int jobId = request.getModel().getInteger("jobId");
+		Job job = this.repository.findOneJobById(jobId);
+		result.setJob(job);
+
+		int workerId = request.getPrincipal().getActiveRoleId();
+		Worker worker = this.repository.findOneWorkerById(workerId);
+		result.setWorker(worker);
+
+		return result;
+	}
+
+	@Override
+	public void validate(final Request<Application> request, final Application entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+	}
+
+	@Override
+	public void create(final Request<Application> request, final Application entity) {
+		assert request != null;
+		assert entity != null;
+
+		this.repository.save(entity);
+
+	}
+
+}
--- src/main/webapp/META-INF/views/worker/application/form.jsp
+++ src/main/webapp/META-INF/views/worker/application/form.jsp
@@ -3,17 +3,23 @@
 <%@taglib prefix = "jstl" uri = "http://java.sun.com/jsp/jstl/core"%>
 <%@taglib prefix = "acme" tagdir = "/WEB-INF/tags"%>
 
-<acme:form readonly="true">
-	<acme:form-textbox code="worker.application.form.reference" path="reference"/>
-	<acme:form-moment code="worker.application.form.moment" path="moment"/>
-	<acme:form-textbox code="worker.application.form.status" path="status"/>
+<acme:form>
+	<acme:form-textbox code="worker.application.form.reference" path="reference"/>	
+	<acme:form-moment code="worker.application.form.moment" path="moment"/>	
+	<acme:form-textbox code="worker.application.form.status" path="status"/>	
 	<acme:form-textarea code="worker.application.form.statement" path="statement"/>
 	<acme:form-textarea code="worker.application.form.skills" path="skills"/>
 	<acme:form-textarea code="worker.application.form.qualifications" path="qualifications"/>
+	
+	<acme:form-submit test="${command == 'create'}" 
+	    code="worker.application.form.button.create" action="/worker/application/create?jobId=${jobId}"/>
+	    
+	<jstl:if test="${command == 'show'}">
 	<acme:form-panel code="worker.application.form.job">
 		<acme:form-textbox code="worker.application.form.job.reference" path="job.reference"/>
 		<acme:form-textbox code="worker.application.form.job.title" path="job.title"/>
 	</acme:form-panel>
-
+	</jstl:if>
+	
 	<acme:form-return code="worker.application.form.return"/>	
 </acme:form>
