e6f1f194d2b202a6b7ef2c5e43b584079c9ae39c
alval <alval@LAPTOP-GKUN0RT8>
2019-12-13 11:12:23 +0100

    UTC_TIMESTAMP to Calendar

--- src/main/java/acme/features/authenticated/job/AuthenticatedJobRepository.java
+++ src/main/java/acme/features/authenticated/job/AuthenticatedJobRepository.java
@@ -2,6 +2,7 @@
 package acme.features.authenticated.job;
 
 import java.util.Collection;
+import java.util.Date;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,8 +14,8 @@ import acme.framework.repositories.AbstractRepository;
 @Repository
 public interface AuthenticatedJobRepository extends AbstractRepository {
 
-	@Query("select j from Job j where j.deadline > utc_timestamp() and j.status = 1")
-	Collection<Job> findManyActiveJobs();
+	@Query("select j from Job j where j.deadline > ?1 and j.status = 1")
+	Collection<Job> findManyActiveJobs(Date d);
 
 	@Query("select j.job from Descriptor j where j.job.id = ?1")
 	Job findOneJobById(int id);
--- src/main/java/acme/features/authenticated/offer/AuthenticatedOfferRepository.java
+++ src/main/java/acme/features/authenticated/offer/AuthenticatedOfferRepository.java
@@ -2,6 +2,7 @@
 package acme.features.authenticated.offer;
 
 import java.util.Collection;
+import java.util.Date;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -15,6 +16,6 @@ public interface AuthenticatedOfferRepository extends AbstractRepository {
 	@Query("select o from Offer o where o.id = ?1")
 	Offer findOneById(int id);
 
-	@Query("select o from Offer o where o.deadline > utc_timestamp()")
-	Collection<Offer> findManyAll();
+	@Query("select o from Offer o where o.deadline > ?1")
+	Collection<Offer> findManyAll(Date d);
 }
