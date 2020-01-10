a4ba607319ee88164abcf55cbc6ce4f04f5b60a3
alval <alval@LAPTOP-GKUN0RT8>
2020-01-11 00:51:57 +0100

    Level B finished

---
+++ src/main/java/acme/components/PhoneFormatter.java
@@ -0,0 +1,72 @@
+
+package acme.components;
+
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.format.Formatter;
+
+import acme.datatypes.Phone;
+import acme.framework.helpers.MessageHelper;
+import acme.framework.helpers.StringHelper;
+
+public class PhoneFormatter implements Formatter<Phone> {
+
+	@Override
+	public String print(final Phone object, final Locale locale) {
+		assert object != null;
+		assert locale != null;
+
+		String result;
+		String countryCodeText, areaCodeText, numberText;
+
+		countryCodeText = String.format("+%d", object.getCountryCode());
+		areaCodeText = object.getAreaCode() == null ? " " : String.format(" (%s) ", object.getAreaCode());
+		numberText = String.format("%s", object.getNumber());
+
+		result = String.format("%s%s%s", countryCodeText, areaCodeText, numberText);
+		return result;
+	}
+
+	@Override
+	public Phone parse(final String text, final Locale locale) throws ParseException {
+		assert !StringHelper.isBlank(text);
+		assert locale != null;
+
+		Phone result;
+		String countryCodeRegex, areaCodeRegex, numberRegex, phoneRegex;
+		Pattern pattern;
+		Matcher matcher;
+		String errorMessage;
+		String countryCodeText;
+		int countryCode;
+		String areaCode, number;
+
+		countryCodeRegex = "\\+\\d{1,3}";
+		areaCodeRegex = "\\d{1,6}";
+		numberRegex = "\\d{1,9}([\\s-]\\d{1,9}){0,4}";
+		phoneRegex = String.format("^\\s*(?<CC>%1$s)(\\s+\\((?<AC>%2$s)\\)\\s+|\\s+)(?<N>%3$s)\\s*$", countryCodeRegex, areaCodeRegex, numberRegex);
+
+		pattern = Pattern.compile(phoneRegex, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+		matcher = pattern.matcher(text);
+
+		if (!matcher.find()) {
+			errorMessage = MessageHelper.getMessage("default.error.conversion", null, "Invalid value", locale);
+			throw new ParseException(errorMessage, 0);
+		} else {
+			countryCodeText = matcher.group("CC");
+			countryCode = Integer.valueOf(countryCodeText);
+			areaCode = matcher.group("AC");
+			number = matcher.group("N");
+
+			result = new Phone();
+			result.setCountryCode(countryCode);
+			result.setAreaCode(areaCode);
+			result.setNumber(number);
+		}
+		return result;
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/tags/form-phone.tag
@@ -0,0 +1,40 @@
+<%@tag language="java" body-content="empty"
+	import="acme.framework.helpers.MessageHelper"
+%>
+
+<%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="acme" tagdir="/WEB-INF/tags"%>
+
+<%@attribute name="path" required="true"%>
+<%@attribute name="code" required="true"%>
+<%@attribute name="placeholder" required="false"%>
+<%@attribute name="readonly" required="false" type="java.lang.Boolean"%>
+
+<jstl:if test="${placeholder == null}">
+	<jstl:set var="placeholder" value="${MessageHelper.getMessage('default.placeholder.phone')}"/>	
+</jstl:if>
+
+<jstl:if test="${readonly == null}">
+	<jstl:set var="readonly" value="false"/>
+</jstl:if>
+
+<div class="form-group">
+	<label for="${path}">
+		<acme:message code="${code}"/>
+	</label>
+	<input 
+		id="${path}" 
+		name="${path}"
+		value="<acme:print value="${requestScope[path]}"/>"
+		type="text"
+		class="form-control"
+		placeHolder="${placeholder}"
+		<jstl:if test="${readonly}">
+       		readonly
+       	</jstl:if>
+	/>
+	<acme:form-errors path="${path}"/>
+	<acme:form-errors path="${path}.countryCode"/>
+	<acme:form-errors path="${path}.areaCode"/>
+	<acme:form-errors path="${path}.number"/>
+</div>
